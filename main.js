f9cc97e0e3bcdcff04f77858d9672b69fcb59145
Pawel Jezior <paweljfrontend@gmail.com>
2021-07-09 20:58:37 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const helloInConsole = 'Hello';
+const someNumber = 123;
+
+console.log('hello')
+console.log(helloInConsole);
+console.log(someNumber);
+
+console.log(`Cześć to po angielsku ${helloInConsole}, a to jest jakas liczba - ${someNumber} :)`);
+
+alert(helloInConsole);
