14749ff76b6e1e5e31c7bee07ceea765c90e664e
Pawel Jezior <paweljfrontend@gmail.com>
2021-07-16 10:37:43 +0200

    format code

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const helloInConsole = 'Hello';
+const helloInConsole = "Hello";
 const someNumber = 123;
 
-console.log('hello')
+console.log("hello");
 console.log(helloInConsole);
 console.log(someNumber);
 
-console.log(`Cześć to po angielsku ${helloInConsole}, a to jest jakas liczba - ${someNumber} :)`);
+console.log(
+  `Cześć to po angielsku ${helloInConsole}, a to jest jakas liczba - ${someNumber} :)`
+);
 
 alert(helloInConsole);
