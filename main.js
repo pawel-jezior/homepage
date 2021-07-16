490a792aecbe28c7e8f125cca06f6ad68f15f251
Pawel Jezior <paweljfrontend@gmail.com>
2021-07-16 13:37:04 +0200

    DOM practice

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ console.log(someNumber);
 console.log(
   `Cześć to po angielsku ${helloInConsole}, a to jest jakas liczba - ${someNumber} :)`
 );
+
+const mainTitle = document.querySelector('.main__title--js');
+
+console.log(mainTitle);
+
+mainTitle.innerHTML = `Cześć to po angielsku ${helloInConsole}, a to jest jakas liczba - ${someNumber} :)`
+
