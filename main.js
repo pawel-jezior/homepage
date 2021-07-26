f03a2dfd87b24323c717c7f182c6c60f78c3c7a7
Pawel Jezior <paweljfrontend@gmail.com>
2021-07-26 17:02:33 +0200

    JS objects practice

--- main.js
+++ main.js
@@ -18,3 +18,22 @@ mainTitle.innerHTML = `Cześć to po angielsku ${helloInConsole}, a to jest jaka
 const greet = (myName) => console.log(`Cześć ${myName}`);
 
 greet('Kamil');
+
+const whiteTable = {
+  height: 60,
+  weight: 90,
+  makePoem: (nameOfBeauty) => console.log(`My Darling ${nameOfBeauty} I don't want live without you...`),
+  color: 'white',
+  nameOfTable: 'White Table',
+  myOwner: 'Pawel is my owner',
+  onWall: true,
+  ownerData: {
+    ownerName: 'Pawel',
+    ownerAge: 29
+  }
+}
+
+console.log(whiteTable.makePoem('Diana'));
+console.log(`Dimensions of table are ${whiteTable.height} x ${whiteTable.weight} :)`);
+
+console.log(whiteTable.ownerData.ownerAge);
